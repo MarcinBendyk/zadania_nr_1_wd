e8c9adf544b56819759869fd14d8602aeaaa20e4
MarcinBendyk <147257953+MarcinBendyk@users.noreply.github.com>
2024-03-09 00:21:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,55 @@
-a = input("podaj zdanie ")
-b = len(a.split())
-print(b)
+#zad1
+#a = input("podaj zdanie ")
+#b = len(a.split())
+#print(b)
+#zad2
+import sys
 
+#a = int(sys.stdin.readline())
+#b = int(sys.stdin.readline())
+#c = int(sys.stdin.readline())
+#sys.stdout.write(f'Wynik: {pow(a,b) + c}')
+
+#slowo = input()
+#rev_sl = slowo[::-1]
+#if  slowo == rev_sl:
+#    print('jest ')
+#else:
+#    print('nie jest ')
+
+#licznik = 0
+#suma = 0
+#for i in range(1,1000):
+#    suma = 0
+#    for j in range(1,i):
+#        if i % j==0:
+#            suma = suma + j
+#    if suma == i:
+#        licznik +=1
+#else:
+#    print(licznik)
+
+
+#lista = [2, 3, 4, 5.5, 6.3, 100, 120.2]
+#
+#for i in lista:
+#    print(pow(i,2))
+
+#i = 0
+#lista = []
+#while i < 10:
+#    liczba = int(input())
+#    if liczba % 2 == 0:
+#        lista.append(liczba)
+#    i += 1
+#print(lista)
+
+#lista = ['e','c',3, 12, 'gg', 1, 4, 'e', 4, 'c', 12]
+#k = {}
+#for i in lista:
+#    k[i] = lista.count(i)
+#for key in k.copy():
+#    if type(key) != int:
+#        del(k[key])
+
+#print(k)
